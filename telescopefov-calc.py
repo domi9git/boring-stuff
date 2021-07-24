74ab6f9bdcba68c725a92420b9a9ef8c314933c9
domi9 <57841773+domi9git@users.noreply.github.com>
2021-07-24 23:27:44 +0300

    def main():

--- telescopefov-calc.py
+++ telescopefov-calc.py
@@ -1,7 +1,11 @@
-a = int(input("enter the focal length of your telescope "))
-b = int(input("enter the aperture of your telescope "))
-c = int(input("enter the focal length of your eyepiece "))
-d = int(input("enter the true fov of your telescope "))
-e = int(input("what kind of barlow/reducer you have (type only the numbers bit (enter 1 for none)) "))
-f = d / (a / c) * 3600 / e
-print("arcsecs: ", f)
+def main():
+ a = int(input("enter the focal length of your telescope "))
+ b = int(input("enter the aperture of your telescope "))
+ c = int(input("enter the focal length of your eyepiece "))
+ d = int(input("enter the true fov of your telescope "))
+ e = int(input("what kind of barlow/reducer you have (type only the numbers bit (enter 1 for none)) "))
+ f = d / (a / c) * 3600 / e
+ print("arcsecs: ", f)
+
+if __name__ == '__main__':
+ main()
